b2ce7784f44909a7aa2e9ca8c8fb006675a2daa0
mionosaur <llmilico@proton.me>
2024-05-31 22:51:29 +0200

    [^] display

--- src/main.c
+++ src/main.c
@@ -1,7 +1,45 @@
 #include <SDL2/SDL.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#define PIXEL_SIZE 16
+#define REFRESH_RATE 60
+
+#define DELAY 1000 / REFRESH_RATE
 
 int main(int argc, char *argv[])
 {
-    SDL_Init(SDL_INIT_VIDEO);
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        fprintf(stderr, "Error on SDL initialization : %s", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window* display = SDL_CreateWindow("Chip-8 emulator", 100, 100, 64 * PIXEL_SIZE, 32 * PIXEL_SIZE, 0);
+    if (display == NULL)
+    {
+        fprintf(stderr, "Error on window creation : %s", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Event event;
+    bool quit = false;
+    while(!quit)
+    {
+        while(SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+                case SDL_QUIT:
+                {
+                    quit = true;
+                }
+            }
+        }
+        SDL_Delay(DELAY);
+    }
+
+    SDL_DestroyWindow(display);
+    SDL_Quit();
     return 0;
 }
