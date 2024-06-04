5e4c43cd5f94b608acd85eb90126f7fb52e4f9a9
mionosaur <llmilico@proton.me>
2024-06-04 23:31:22 +0200

    [+] set instructions

--- test_opcde_reading.c
+++ test_opcde_reading.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdint.h>
-#include <time.h>
+#include <sys/time.h>
+#include <unistd.h>
 
 #define MEMORY_SIZE 4096
-#define MS_PER_FRAME 16 // 60hz
+#define MS_PER_FRAME 500 // 60hz
 
 int main()
 {
@@ -17,6 +18,9 @@ int main()
         chip_ram[i] = 0;
     }
 
+    uint8_t V[16]; // Registers
+    uint16_t I;
+
     int bytes_read = fread(chip_ram + 0x200, 1, MEMORY_SIZE - 0x200, file);
     fclose(file);
 
@@ -29,20 +33,10 @@ int main()
     while (!quit)
     {
         gettimeofday(&start_time, NULL);
-        uint16_t opcode = chip_ram[pc] << 8 | chip_ram[i+1];
 
+        uint16_t opcode = chip_ram[pc] << 8 | chip_ram[pc+1];
+        printf("%4X ", opcode);
 
-        gettimeofday(&end_time, NULL);
-        elapsed_ms = (end_time.tv_sec - start_time.tv_sec) * 1000 + (end_time.tv_usec - start_time.tv_usec) / 1000;
-
-        if (elapsed_ms < MS_PER_FRAME) {
-            usleep((MS_PER_FRAME - elapsed_ms) * 1000);
-        }
-    }
-    /*
-    for (int i = 0; i < bytes_read; i += 2)
-    {
-        uint16_t opcode = message[i] << 8 | message[i+1];
         switch ((opcode & 0xF000) >> 12) // First nibble
         {
             case 0X0:
@@ -59,7 +53,7 @@ int main()
             }
             case 0X1:
             {
-                printf("Jump to location %3X\n", (opcode & 0x0FFF));
+                pc = (opcode & 0x0FFF);
                 break;
             }
             case 0X2:
@@ -82,14 +76,13 @@ int main()
                 uint8_t X, Y;
                 X = (opcode & 0x0F00) >> 8;
                 Y = (opcode & 0x00F0) >> 4;
-
                 printf("Skip next instruction if V%1X = V%1X\n", X, Y);
-
                 break;
             }
             case 0X6:
             {
-                printf("Set V%1X = %2X\n", (opcode & 0x0F00) >> 8, (opcode & 0x00FF));
+                V[(opcode & 0x0F00) >> 8] = (opcode & 0x00FF);
+                pc += 2;
                 break;
             }
             case 0X7:
@@ -103,7 +96,6 @@ int main()
                 X = (opcode & 0x0F00) >> 8;
                 Y = (opcode & 0x00F0) >> 4;
                 operation = (opcode & 0x000F);
-
                 switch (operation)
                 {
                     case 0x0:
@@ -165,13 +157,13 @@ int main()
                 uint8_t X, Y;
                 X = (opcode & 0x0F00) >> 8;
                 Y = (opcode & 0x00F0) >> 4;
-
                 printf("Skip next instruction if V%1X != V%1X\n", X, Y);
                 break;
             }
             case 0XA:
             {
-                printf("Set I = %3X\n", (opcode & 0x0FFF));
+                I = (opcode & 0x0FFF);
+                pc += 2;
                 break;
             }
             case 0XB:
@@ -210,9 +202,8 @@ int main()
             }
             case 0XF:
             {
-                uint4_t X = (opcode & 0x0F00) >> 8;
+                uint8_t X = (opcode & 0x0F00) >> 8;
                 uint8_t instruction = opcode & 0x00FF;
-
                 switch (instruction)
                 {
                     case 0x07:
@@ -260,8 +251,20 @@ int main()
                 break;
             }   
         }
+        printf("\n");
+
+        gettimeofday(&end_time, NULL);
+        elapsed_ms = (end_time.tv_sec - start_time.tv_sec) * 1000 + (end_time.tv_usec - start_time.tv_usec) / 1000;
+        if (elapsed_ms < MS_PER_FRAME)
+        {
+            usleep((MS_PER_FRAME - elapsed_ms) * 1000);
+        }
     }
-    printf("\n");*/
+    /*
+    for (int i = 0; i < bytes_read; i += 2)
+    {
+        uint16_t opcode = message[i] << 8 | message[i+1];
+        */
 
     return 0;
 }
